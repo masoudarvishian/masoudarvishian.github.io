40014837ea86f8821dfb680599b53ddff228d092
Masoud Darvishian <masoudarvishian@gmail.com>
2018-01-17 11:29:21 +0330

    html5 tags added, myScripts.js file added

---
+++ myScripts.js
@@ -0,0 +1,79 @@
+$(function () {
+    $(".typed1").typed({
+        strings: [
+            "Hi"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 30,
+        // time before typing starts
+        startDelay: 2400
+    });
+    $(".typed2").typed({
+        strings: [
+            ", I'm Masoud Darvishian  and I make video games."
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 30,
+        // time before typing starts
+        startDelay: 3200
+    });
+    $(".typed3").typed({
+        strings: [
+            "If you would like to find me online follow the links below!"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 20,
+        // time before typing starts
+        startDelay: 8200
+    });
+    $(".typed4").typed({
+        strings: [
+            "github"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 20,
+        // time before typing starts
+        startDelay: 13200
+    });
+    $(".typed5").typed({
+        strings: [
+            "stackoverflow"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 20,
+        // time before typing starts
+        startDelay: 14200
+    });
+    $(".typed6").typed({
+        strings: [
+            "twitter"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 20,
+        // time before typing starts
+        startDelay: 15200
+    });
+    $(".typed7").typed({
+        strings: [
+            "masoudarvishian@gmail.com"
+        ],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 30,
+        // time before typing starts
+        startDelay: 15700
+    });
+});
