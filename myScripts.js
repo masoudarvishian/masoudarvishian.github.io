efc29501ba8fd7e560b0ca9d17e68028dc76f2fd
Masoud Darvishian <masoudarvishian@gmail.com>
2018-09-29 16:19:49 +0330

    Update myScripts.js

--- myScripts.js
+++ myScripts.js
@@ -1,7 +1,7 @@
 $(function () {
     $(".typed1").typed({
         strings: [
-            "Greeting"
+            "Greeting!"
         ],
         // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
         stringsElement: null,
@@ -12,7 +12,7 @@ $(function () {
     });
     $(".typed2").typed({
         strings: [
-            "! I am Masoud Darvishian; a full-stack developer."
+            " I am Masoud Darvishian; a full-stack developer."
         ],
         // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
         stringsElement: null,
